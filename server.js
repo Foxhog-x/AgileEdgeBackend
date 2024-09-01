9f8ca8f1d7655b32925308e950abe9b16d2485bd
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-09-01 23:58:28 +0530

    local env updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const getConnection = require("./db.js");
 const jwtVerify = require("./middleware/jwtVerfiy.js");
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: ["https://agile-edge-app.netlify.app"],
+  origin: ["https://agile-edge-app.netlify.app", "http://localhost:5173"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
