af52312ceaea2ab110f55847f28f52447af28924
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-09-10 20:52:35 +0530

    sql connection limit increase

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const getConnection = async () => {
     password: process.env.MYSQL_PASSWORD || "",
     database: process.env.MYSQL_DATABASE || "agile",
     waitForConnections: true,
-    connectionLimit: 600,
+    connectionLimit: 1000,
     connectTimeout: 10000,
   });
 
